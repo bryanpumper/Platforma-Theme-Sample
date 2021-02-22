b520b844ac03fe856ca03804dd344f17b17e3c0a
Bryan Urfano <bryan@consultrecruitment.co.nz>
2021-02-22 21:11:11 +0800

    Removed footer contents

--- footer.php
+++ footer.php
@@ -13,18 +13,19 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'platforma-theme-sample' ) ); ?>">
+			<!--<a href="<?php //echo esc_url( __( 'https://wordpress.org/', 'platforma-theme-sample' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'platforma-theme-sample' ), 'WordPress' );
+				//printf( esc_html__( 'Proudly powered by %s', 'platforma-theme-sample' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'platforma-theme-sample' ), 'platforma-theme-sample', '<a href="http://underscores.me/">Bryan Urfano</a>' );
+				//printf( esc_html__( 'Theme: %1$s by %2$s.', 'platforma-theme-sample' ), 'platforma-theme-sample', '<a href="http://underscores.me/">Bryan Urfano</a>' );
 				?>
 		</div><!-- .site-info -->
+        </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
