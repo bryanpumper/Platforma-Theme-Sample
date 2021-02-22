aec56f2a2c6c7ad407e62e407bf2d282029a7819
Bryan Urfano <bryan@consultrecruitment.co.nz>
2021-02-22 19:31:57 +0800

    initial commit - dev started

---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Platforma_Theme_Sample
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
